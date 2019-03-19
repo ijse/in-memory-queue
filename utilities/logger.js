84d36f64206a63caf37f9eb2246ae4c3e94c465c
Li Yi <475440+ijse@users.noreply.github.com>
2019-03-19 21:37:45 +0800

    Update logger.js

--- utilities/logger.js
+++ utilities/logger.js
@@ -2,9 +2,11 @@
 
 const Winston = require('winston');
 
+const DEBUG = (process.env.DEBUG || '').match(/in-memory-queue/)
+
 const Logger = scope => {
     return new Winston.Logger({
-        level: 'info',
+        level: DEBUG ? 'info' : 'error',
         transports: [
             new (Winston.transports.Console)({
                 timestamp: true,
